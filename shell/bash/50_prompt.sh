d442518272b1658bb9255d15e54bde2792b27b56
Kyle Huang <kyle@yellowaxe.com>
2016-07-23 23:54:57 -0500

    simplified bash prompt - added multiline and git

--- shell/bash/50_prompt.sh
+++ shell/bash/50_prompt.sh
@@ -1,1 +1,19 @@
-PS1="\[\e[0;31m\][\[\e[0;33m\]\u\[\e[0;31m\]@\[\e[0;33m\]\h \[\e[0;36m\]\W\[\e[0;31m\]]\\$ \[\e[0m\]"
+function __set_prompt_command() {
+  local  reset='\[\e[0m\]'
+  local  black='\[\e[0;30m\]'
+  local    red='\[\e[0;31m\]'
+  local  green='\[\e[0;32m\]'
+  local yellow='\[\e[0;33m\]'
+  local   blue='\[\e[0;34m\]'
+  local purple='\[\e[0;35m\]'
+  local   cyan='\[\e[0;36m\]'
+  local  white='\[\e[0;37m\]'
+
+  local userhost="${yellow}\u${red}@${yellow}\h"
+  local workdir="${cyan}\w"
+  local gitstat='$(__git_ps1 " (%s)")'
+  local time="${cyan}\t"
+
+  PS1="${red}[${userhost} ${workdir}${red}]${blue}${gitstat}\n${red}[${time}${red}] ${red}\\$ ${reset}"
+}
+__set_prompt_command
