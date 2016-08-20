a1f0d77634ee6520935b22febfb4b926b6ee2638
Kyle Huang <kyle@yellowaxe.com>
2016-08-20 01:49:26 -0500

    update git prompt to show upstream diff

--- shell/50_git_prompt.sh
+++ shell/50_git_prompt.sh
@@ -2,5 +2,6 @@ if [[ -e $DOTFILES/thirdparty/git-prompt/git-prompt.sh ]]; then
   GIT_PS1_SHOWUNTRACKEDFILES=yes
   GIT_PS1_SHOWDIRTYSTATE=yes
   GIT_PS1_SHOWSTASHSTATE=yes
+  GIT_PS1_SHOWUPSTREAM=verbose
   source $DOTFILES/thirdparty/git-prompt/git-prompt.sh
 fi
