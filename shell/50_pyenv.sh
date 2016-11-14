26dacb12b4efc4edaa7b4191ef72a81636ac57ee
Kyle Huang <kyle@yellowaxe.com>
2016-11-14 02:34:34 -0600

    updated pyenv config. added toml ft plugin

--- shell/50_pyenv.sh
+++ shell/50_pyenv.sh
@@ -1,5 +1,8 @@
 if [[ -e $DOTFILES/thirdparty/pyenv/bin/pyenv ]]; then
   export PATH=$DOTFILES/thirdparty/pyenv/bin:$PATH
+  # https://github.com/yyuu/pyenv/wiki#how-to-build-cpython-with-framework-support-on-os-x
+  # this is need by some tools (like YouCompleteMe plugin)
+  export PYTHON_CONFIGURE_OPTS="--enable-framework"
   eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
 fi
