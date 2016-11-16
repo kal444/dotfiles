7cfb45d609e304c4437f4bf4b15278f9f575d733
Kyle Huang <kyle@yellowaxe.com>
2016-11-16 00:02:50 -0600

    added back home drive usage

--- bin/bootstrap.cmd
+++ bin/bootstrap.cmd
@@ -2,8 +2,7 @@
 setlocal
 
 set DOTREPO=https://github.com/kal444/dotfiles.git
-REM if not exist "%HOME%" set HOME=%HOMEDRIVE%%HOMEPATH%
-REM what if I don't want to use the network home drive?
+if not exist "%HOME%" set HOME=%HOMEDRIVE%%HOMEPATH%
 if not exist "%HOME%" set HOME=%USERPROFILE%
 set DOTFILES=%HOME%\.dotfiles
 
