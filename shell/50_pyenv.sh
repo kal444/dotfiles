74670f87e8ad49549a6d49ea4fbd969199328358
Kyle Huang <kyle@yellowaxe.com>
2017-09-06 00:07:15 -0500

    disabled build framework flag for pyenv

--- shell/50_pyenv.sh
+++ shell/50_pyenv.sh
@@ -3,7 +3,8 @@ if [[ "$DISABLE_PYENV" != "1" ]]; then
     export PATH=$DOTFILES/thirdparty/pyenv/bin:$PATH
     # https://github.com/yyuu/pyenv/wiki#how-to-build-cpython-with-framework-support-on-os-x
     # this is need by some tools (like YouCompleteMe plugin)
-    export PYTHON_CONFIGURE_OPTS="--enable-framework"
+    # commenting this out for now since I'm not using YouCompleteMe anymore
+    # export PYTHON_CONFIGURE_OPTS="--enable-framework"
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
   fi
