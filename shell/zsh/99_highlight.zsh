83a6bcbde71c9d2514b442ad866c2f56fd9b4fc9
Kyle Huang <kyle@yellowaxe.com>
2018-12-15 01:33:57 -0600

    Modify cursor color during highlight

--- shell/zsh/99_highlight.zsh
+++ shell/zsh/99_highlight.zsh
@@ -2,6 +2,7 @@ if [[ "$DISABLE_ZSH_SYNTAX_HIGHLIGHT" != 1 ]]; then
   source $DOTFILES/thirdparty/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
   source $DOTFILES/thirdparty/zsh-syntax-highlighting-filetypes/zsh-syntax-highlighting-filetypes.zsh
   ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+  ZSH_HIGHLIGHT_STYLES[cursor]='bg=blue'
 fi
 
 if [[ "$DISABLE_ZSH_AUTO_SUGGESTION" != 1 ]]; then
