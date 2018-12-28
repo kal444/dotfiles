0b4422af46d189d58fac6b5aaa41bf1f85abc7f7
Kyle Huang <kyle@yellowaxe.com>
2018-12-28 00:30:55 -0600

    Fix typo

--- shell/bash/50_completion.sh
+++ shell/bash/50_completion.sh
@@ -1,8 +1,8 @@
 # Add tab completion for many Bash commands
 if [ -f /usr/local/etc/bash_completion ]; then
   source /usr/local/etc/bash_completion;
-if [ -f /usr/share/bash-completion/bash_completion ]; then
-  source / usr/share/bash-completion/bash_completion;
+elif [ -f /usr/share/bash-completion/bash_completion ]; then
+  source /usr/share/bash-completion/bash_completion;
 elif [ -f /etc/bash_completion ]; then
   source /etc/bash_completion;
 fi;
