07eeb914375f7e93626dbf385c516f5505cee11a
Kyle Huang <kyle@yellowaxe.com>
2019-10-26 18:40:31 -0500

    Update submodule versions and removed file types highlighting

--- shell/zsh/99_highlight.zsh
+++ shell/zsh/99_highlight.zsh
@@ -1,12 +1,11 @@
-if [[ "$DISABLE_ZSH_SYNTAX_HIGHLIGHT" != 1 ]]; then
-  source $DOTFILES/thirdparty/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-  source $DOTFILES/thirdparty/zsh-syntax-highlighting-filetypes/zsh-syntax-highlighting-filetypes.zsh
-  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
-  ZSH_HIGHLIGHT_STYLES[cursor]='bg=blue'
-fi
-
 if [[ "$DISABLE_ZSH_AUTO_SUGGESTION" != 1 ]]; then
   source $DOTFILES/thirdparty/zsh-autosuggestions/zsh-autosuggestions.zsh
   ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
   ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=gray'
+fi
+
+if [[ "$DISABLE_ZSH_SYNTAX_HIGHLIGHT" != 1 ]]; then
+  source $DOTFILES/thirdparty/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+  ZSH_HIGHLIGHT_STYLES[cursor]='bg=blue'
 fi
