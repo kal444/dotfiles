6c96ee9294d213bbebfc1b3255319c02e2ac1d0e
Kyle Huang <kyle@yellowaxe.com>
2021-01-27 10:40:45 -0600

    Update terminfo source url

--- bin/build-terminfo.sh
+++ bin/build-terminfo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -O https://invisible-mirror.net/archives/ncurses/terminfo.src.gz
+curl -O https://invisible-island.net/datafiles/current/terminfo.src.gz
 gunzip terminfo.src.gz
 tic terminfo.src
 rm terminfo.src
