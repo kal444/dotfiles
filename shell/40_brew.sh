03ea6a2ded57bc1fa26930eae17ad32d7db73c82
Kyle Huang <kyle@yellowaxe.com>
2022-09-27 08:51:09 -0500

    Add brew usage

--- shell/40_brew.sh
+++ shell/40_brew.sh
@@ -2,4 +2,6 @@ if [[ -d ~/.linuxbrew ]]; then
   eval $(~/.linuxbrew/bin/brew shellenv)
 elif [[ -d /home/linuxbrew/.linuxbrew ]]; then
   eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+elif [[ -d /opt/homebrew ]]; then
+  eval $(/opt/homebrew/bin/brew shellenv)
 fi
